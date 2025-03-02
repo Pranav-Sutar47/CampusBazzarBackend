b263131dff180c365a3431432f03898abd013718
Pranav <pranavsutar4747@gmail.com>
2025-03-02 13:43:31 +0530

    Backend Host

--- index.js
+++ index.js
@@ -18,6 +18,8 @@ require('./Models/Config');
 
 const PORT = process.env.PORT || 5000;
 
+const SERVER_URL = `http://localhost:${PORT}/ping`;
+
 // User Routes
 app.use('/api/user',require('./Routes/User'));
 
@@ -37,7 +39,7 @@ app.listen(PORT,'0.0.0.0',()=>{
     console.log(`Server Up and Listen on ${PORT}`);
     setInterval(() => {
             axios
-            .get(URL/ping)
+            .get(SERVER_URL)
             .then(() => console.log("Self-ping successful"))
             .catch((err) => console.error("Self-ping failed:", err.message));
     }, 3 *60*1000);
